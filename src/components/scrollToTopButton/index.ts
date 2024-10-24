4d77a5ed82dcd94ca9df011877ced9aeffcb9fea
dunglh-it <dunglh.it@gmail.com>
2024-10-24 23:52:23 +0700

    Fix

--- src/components/scrollToTopButton/index.ts
+++ src/components/scrollToTopButton/index.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-unresolved
-import ScrollToTopButton from './scrollToTopButton'
+import ScrollToTopButton from './ScrollToTopButton'
 
 export default ScrollToTopButton
